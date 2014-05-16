546a9317a19efcae7f409d0c7e3cf8d9714a7341
K <celfrost@hotmail.com>
2014-05-16 13:51:48 -0400

    Playing w/ hashes

--- cash_register.rb
+++ cash_register.rb
@@ -1,5 +1,22 @@
+#SKU,name,wholesale_price,retail_price
+#120945,Light,2.50,5
+#679340,Medium,3.25,7.50
+#328745,Bold,4.75,9.75
+#puts sale_items['120945'] ----=>Light
+#puts sale_prices -------------=>{2.5=>5.0, 3.25=>7.5, 4.75=>9.75}
+#name = gets.chomp
+# if phone_book.has_key?(name)
+#   puts "Cell: #{phone_book[name][:cell]}"
+#   puts "Work: #{phone_book[name][:work]}"
+# else
+#   puts "Name not found."
+#end
 require "csv"
 require "pry"
+
+amount = []
+menu= {}
+id = 1
 
 def sum_array(i)
 	total = 0
@@ -10,15 +27,15 @@ def sum_array(i)
 	puts "The total so far is $#{total}"
 end
 
-amount = []
-sale_items = {}
-sale_prices = {}
-
 CSV.foreach('products.csv', headers: true) do |row|
 	sku = row["SKU"]
 	name = row["name"]
 	wholesale_price = row["wholesale_price"].to_f
 	retail_price = row["retail_price"].to_f
-	sale_items[sku] = name
-	sale_prices[wholesale_price] = retail_price
+	menu[id] = {id: id, sku: sku, name: name, wholesale_price: wholesale_price, retail_price: retail_price}
+	id += 1
 end
+
+puts menu
+puts 'This is item #menu[1].'
+puts 'This is the item'
